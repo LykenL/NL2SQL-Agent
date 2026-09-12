cd768ffcf5e37f14dd5d4b1a416cbc7b3351e4ee
Junxian Lyken Lin <lyken35@student.ubc.ca>
2026-09-12 00:18:16 -0700

    fix: provide a pure wsgi application in api/index.py to flawlessly bypass vercel python entrypoint detection without dependencies

--- api/index.py
+++ api/index.py
@@ -1,9 +1,4 @@
-from http.server import BaseHTTPRequestHandler
-
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write('Dummy Vercel Python Handler'.encode('utf-8'))
-        return
+# Pure standard library WSGI app to satisfy Vercel AST validation
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [b'Dummy Vercel Python Handler']
